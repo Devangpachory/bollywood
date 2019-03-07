9bb36f552382cecbdfb66bdad8d5c68d9f8e837c
devangpachory <devang.pachory_mca18@gla.ac.in>
2019-03-07 10:48:10 +0530

    put edittext

--- app/src/main/java/com/devangpachory/bollywood/MainActivity.java
+++ app/src/main/java/com/devangpachory/bollywood/MainActivity.java
@@ -1,13 +1,39 @@
 package com.devangpachory.bollywood;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences sharedPreferences = getSharedPreferences("com.devangpachory.bollywood", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("name", editText.getText().toString().trim());
+        editor.putBoolean("bool", true);
+        editor.apply();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sharedPreferences = getSharedPreferences("com.devangpachory.bollywood", MODE_PRIVATE);
+        String s = sharedPreferences.getString("name", "user");
+        Boolean remember = sharedPreferences.getBoolean("bool", false);
+        Toast.makeText(this, "bool" + remember, Toast.LENGTH_SHORT).show();
+        editText.setText(s);
+
     }
 }
